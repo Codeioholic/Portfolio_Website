206fdc97227785b7f3ab637dc47fe98ade28e13a
Ujjwal Kumar <kumarujjwal7264@gmail.com>
2023-09-05 14:57:06 +0530

    Initial commit

---
+++ script.js
@@ -0,0 +1,43 @@
+// Scroll sections active kink
+let sections = document.querySelectorAll('section');
+let navlinks = document.querySelectorAll('header nav a');
+
+
+
+window.onscroll = () => {
+    sections.forEach(sec => {
+        let top = window.scrollY;
+        let offset = sec.offsetTop - 150;
+        let height = sec.offsetHeight;
+        let id = sec.getAttribute('id');
+
+        if(top >= offset && top < offset + height) {
+            navlinks.forEach(links => {
+                links.classList.remove('active');
+                document.querySelector('header nav a[href*=' + id + ']').classList.add('active');
+            });
+        };
+    });
+
+    // Sticky navigation bar
+    let header = document.querySelector('.header');
+    
+    header.classList.toggle('sticky', window.scrollY > 100);
+};
+
+// top button scroll
+var swiper = new Swipper(".mySwiper", {
+    slidesPerView: 1,
+    spaceBetween: 50,
+    loop: true,
+    grabCursor: true,
+    pagination: {
+        el: ".swiper-pagination",
+        clickable: true,
+    },
+    navigation: {
+        nextEl: ".swiper-button-next",
+        prevEl: ".swiper-button-prev",
+    },
+});
+
